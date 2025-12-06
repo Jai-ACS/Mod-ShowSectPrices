1d43f54795cb605ec3fa9e9d3bcd44bcf61e1ce7
Jai <jyai.san+acs@gmail.com>
2025-12-07 01:37:52 +0800

    Update and rename ShowGridMoreInfoMod.lua to ShowSectPrices.lua

---
+++ Scripts/ShowSectPrices.lua
@@ -0,0 +1,21 @@
+local Mod = GameMain:GetMod("Jai_ShowSectPrices")
+
+function ShowGridMoreInfoMod:OnInit()
+	local tbEventMod = GameMain:GetMod("_Event")
+	tbEventMod:RegisterEvent(g_emEvent.WindowEvent, self.OnWindowEvent, self)
+end
+
+function ShowGridMoreInfoMod:OnRender(_)
+	self:ShowGridInfo()
+end
+
+function ShowGridMoreInfoMod:OnWindowEvent(pThing, pObjs)
+	local pWnd = pObjs[0]
+	local iArg = pObjs[1]
+	if pWnd == CS.Wnd_SchoolTrade.Instance and iArg == 1 then
+		print("showing")
+		pWnd.UIInfo.m_itemvalue.visible = true
+		pWnd.UIInfo.m_friendpontvalue.visible = true
+		pWnd.UIInfo.m_friendpontvalue.y = pWnd.UIInfo.m_itemvalue.y - pWnd.UIInfo.m_friendpontvalue.actualHeight
+	end
+end
