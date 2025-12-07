f21e95abb14496a52287e4ab77d88ce621ca9e8a
Jai <jyai.san+acs@gmail.com>
2025-12-07 08:04:11 +0800

    Update ShowSectPrices.lua

--- Scripts/ShowSectPrices.lua
+++ Scripts/ShowSectPrices.lua
@@ -12,12 +12,14 @@ end
 function Mod:OnWindowEvent(pThing, pObjs)
 	local pWnd = pObjs[0]
 	local iArg = pObjs[1]
-	if pWnd == CS.Wnd_SchoolTrade.Instance and iArg == 1 then
+	--if pWnd == CS.Wnd_SchoolTrade.Instance and iArg == 1 then
+	if pWnd == CS.Wnd_SchoolTrade.Instance then
 		-- print("showing")
 		pWnd.UIInfo.m_itemvalue.visible = true
 		pWnd.UIInfo.m_friendpontvalue.visible = true
 		pWnd.UIInfo.m_friendpontvalue.y = pWnd.UIInfo.m_itemvalue.y - pWnd.UIInfo.m_friendpontvalue.actualHeight
 	end
 end
+
 
 
