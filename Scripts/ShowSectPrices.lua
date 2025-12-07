3b85e3451034aded7539f733e47babf8a1d0d51b
Jai <jyai.san+acs@gmail.com>
2025-12-07 21:25:14 +0800

    Update ShowSectPrices.lua

--- Scripts/ShowSectPrices.lua
+++ Scripts/ShowSectPrices.lua
@@ -17,12 +17,18 @@ function Mod:OnWindowEvent(pThing, pObjs)
 		-- pWnd.UIInfo.m_itemvalue.visible = true
 		-- pWnd.UIInfo.m_friendpontvalue.visible = true
 		-- pWnd.UIInfo.m_friendpontvalue.y = pWnd.UIInfo.m_itemvalue.y - pWnd.UIInfo.m_friendpontvalue.actualHeight
+
+		self:showPrices(pWnd.UIInfo)
 		pWnd.UIInfo.m_n51.onClickItem:Add(
 			function()
-				pWnd.UIInfo.m_itemvalue.visible = true
-				pWnd.UIInfo.m_friendpontvalue.visible = true
-				pWnd.UIInfo.m_friendpontvalue.y = pWnd.UIInfo.m_itemvalue.y - pWnd.UIInfo.m_friendpontvalue.actualHeight
+				self:showPrices(pWnd.UIInfo)
 			end
 		)
 	end
+end
+
+function Mod:showPrices(uiInfo)
+	uiInfo.m_itemvalue.visible = true
+	uiInfo.m_friendpontvalue.visible = true
+	uiInfo.m_friendpontvalue.y = uiInfo.m_itemvalue.y - uiInfo.m_friendpontvalue.actualHeight
 end
