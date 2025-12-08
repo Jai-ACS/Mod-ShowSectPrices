2e1139bd4a163cf5434ce9b182fcfbdf443816b6
Jai <jyai.san+acs@gmail.com>
2025-12-08 10:04:50 +0800

    Update ShowSectPrices.lua

--- Scripts/ShowSectPrices.lua
+++ Scripts/ShowSectPrices.lua
@@ -9,17 +9,21 @@ function Mod:OnWindowEvent(pThing, pObjs)
 	local pWnd = pObjs[0]
 	local iArg = pObjs[1]
 	if pWnd == CS.Wnd_SchoolTrade.Instance and iArg == 1 then
-		self:showPrices(pWnd.UIInfo)
-		pWnd.UIInfo.m_n64.m_n63.onClickItem:Add(
-			function()
-				self:showPrices(pWnd.UIInfo)
-			end
-		)
+		if pWnd.UIInfo ~= nil then
+			self:showPrices(pWnd.UIInfo)
+			pWnd.UIInfo.m_n64.m_n63.onClickItem:Add(
+				function()
+					self:showPrices(pWnd.UIInfo)
+				end
+			)
+		end
 	end
 end
 
 function Mod:showPrices(uiInfo)
-	uiInfo.m_itemvalue.visible = true
-	uiInfo.m_friendpontvalue.visible = true
-	uiInfo.m_friendpontvalue.y = uiInfo.m_itemvalue.y - uiInfo.m_friendpontvalue.actualHeight
+	if uiInfo ~= nil then
+		uiInfo.m_itemvalue.visible = true
+		uiInfo.m_friendpontvalue.visible = true
+		uiInfo.m_friendpontvalue.y = uiInfo.m_itemvalue.y - uiInfo.m_friendpontvalue.actualHeight
+	end
 end
